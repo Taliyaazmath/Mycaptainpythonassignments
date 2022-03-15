3a81df0173a3c4a272e8738b59ce64dda8a68300
Taliya <101127449+Taliyaazmath@users.noreply.github.com>
2022-03-15 21:22:04 +0530

    Add files via upload

---
+++ abc1.py
@@ -0,0 +1,13 @@
+#code to find the area of circle taking the radius as input
+radius=float(input("input the radius of the circle:"))
+#area of the circle
+area = 3.14 * radius * radius
+print("The area of the circle with radius ", radius ,"is :", area)
+
+
+#second code
+filename=input("input the filename:")
+f=filename.split(".")
+ext = f[-1]
+print("extension of the file is :" ,ext)
+
