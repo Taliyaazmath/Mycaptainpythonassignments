7d31c1656e992facf9eaeef2762ea5d1177af5cb
Taliya <101127449+Taliyaazmath@users.noreply.github.com>
2022-03-15 22:04:14 +0530

    Delete abc.py

--- abc.py
+++
@@ -1,5 +0,0 @@
-#code to find the area of circle taking the radius as input
-radius=float(input("input the radius of the circle:"))
-#area of the circle
-area = 3.14 * radius * radius
-print("The area of the circle with radius ", radius ,"is :", area)
