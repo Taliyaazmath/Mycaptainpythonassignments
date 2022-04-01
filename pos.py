77c9c081a4fe5870dcdd9d7a9c5cc2b2e94ad901
Taliya <101127449+Taliyaazmath@users.noreply.github.com>
2022-04-01 17:18:46 +0530

    Add files via upload

---
+++ pos.py
@@ -0,0 +1,22 @@
+#all positive numbers in a range
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+
+pos_list1=[]
+pos_list2=[]
+
+for n1 in list1:
+    if n1>0:
+        pos_list1.append(n1)
+        
+for n2 in list2:
+    if n2>0:
+        pos_list2.append(n2)
+
+print("positive element of the list1:")
+print(pos_list1)
+
+print("positive element of the list2:")
+print(pos_list2)
+
+
